810681fe401323c211001f754932c29bb676ff84
ridwaanhall <erbyl14@gmail.com>
2024-01-10 07:11:31 +0700

    print tutorial

---
+++ 01_python/01_prints.py
@@ -0,0 +1,42 @@
+# print tutorial in python
+print("hello")
+print('hello')
+
+# concatenation
+print("hello" + "world")
+
+# variables
+a = "hello"
+b = "world"
+print(a + b)
+
+# numbers
+a = 10
+b = 20
+print(a + b)
+
+# floats
+a = 10.5
+b = 20.5
+print(a + b)
+
+# booleans
+a = True
+b = False
+print(a + b)
+
+# lists
+a = [1, 5, 3]
+b = [4, 5, 6]
+print(a + b)
+
+# tuples
+a = (1, 5, 3)
+b = (4, 5, 6)
+print(a + b)
+
+# dictionaries
+a = {"a": 1, "b": 2}
+b = {"c": 3, "d": 4}
+a.update(b)
+print(a)
